bf828414705914886317a1a8e014625ebd59fefd
邓捷w99503 <99503@sangfor.com>
2021-05-18 19:15:03 +0800

    Updated at 2021/05/18 19:15

--- hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d
+++
@@ -1,5 +0,0 @@
-/home/dj/Desktop/rust_learning/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77: src/main.rs
-
-/home/dj/Desktop/rust_learning/hello-rust/target/debug/deps/hello_rust-6c97e69e55771b77.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/dj/Desktop/rust_learning/hello-rust/target/debug/hello-rust: /home/dj/Desktop/rust_learning/hello-rust/src/main.rs
