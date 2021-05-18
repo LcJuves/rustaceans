5b4f127aec9876e604693793d5b47400ec44a9f7
邓捷w99503 <99503@sangfor.com>
2021-05-18 18:56:58 +0800

    first commit

---
+++ gitp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# Created at 2021/4/6 21:26.
+# @author Liangcheng Juves
+
+
+tm=$(date "+%Y/%m/%d %H:%M")
+base_dir="$(cd "$(dirname "$0")"; pwd)"
+
+git -C ${base_dir} add ${base_dir}
+git -C ${base_dir} commit -m "Updated at ${tm}"
+git -C ${base_dir} push -u origin main
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/dj/Desktop/rust_learning/hello-rust/target/debug/hello-rust: /home/dj/Desktop/rust_learning/hello-rust/src/main.rs
