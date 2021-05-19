95d00b979aeee539989a67edb3aad2b41f4f5850
Liangcheng Juves <liangchengj@outlook.com>
2021-05-20 01:04:30 +0800

    Updated at 2021/05/20 01:04

--- hex-string/src/main.rs
+++ hex-string/src/main.rs
@@ -2,14 +2,16 @@ use std::char;
 
 fn main() {
     let my_name = "Liangcheng Juves".to_string();
-    let bytes = my_name.as_bytes();
-    println!("{}", HexString::from(bytes));
+    let hex = HexString.from(my_name.as_bytes());
+    println!("Convert my_name to hex string is {}", hex);
+    // let bytes = HexString.parse(hex);
+    // println!("Convert hex string to my_name is {:?}", bytes);
 }
 
 struct HexString;
 
 impl HexString {
-    pub fn from(bytes: &[u8]) -> String {
+    pub fn from(&self, bytes: &[u8]) -> String {
         let mut result = String::from("");
         let radix: u32 = 16;
         for byte in bytes {
@@ -20,8 +22,16 @@ impl HexString {
         result
     }
 
-    pub fn parse(@hex: String) -> &'static [u8] {
-        let result: [u8; hex.len() / 2];
-        result
-    }
+    // pub fn parse(&self, |hex| hex: String) -> Vec<u8> {
+    //     const result_arrlen = hex.len() / 2;
+    //     let result: Vec<u8> = Vec::new();
+    //     for i in 0..result_arrlen {
+    //         let digit: u32;
+    //         digit = hex.chars().nth(2 * i).to_digit(16);
+    //         digit <<= 4;
+    //         digit += hex.chars().nth(2 * i + 1).to_digit(16);
+    //         result.push(digit)
+    //     }
+    //     result
+    // }
 }
