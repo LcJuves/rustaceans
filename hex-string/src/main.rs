abde3f9bb7dfe621bf3789f31d3f77d2c5c1cb46
Liangcheng Juves <liangchengj@outlook.com>
2021-07-09 00:09:15 +0800

    Good

--- hex-string/src/main.rs
+++ hex-string/src/main.rs
@@ -3,36 +3,63 @@ use std::char;
 fn main() {
     let my_name = "Liangcheng Juves".to_string();
     let hex = HexString.from(my_name.as_bytes());
+    println!("Convert \"{}\" to hex string is {}", my_name, hex);
+
+    let bytes = HexString.parse(&hex);
+    println!("Convert \"{}\" to bytes is {:?}", hex, bytes);
+    println!(
+        "Prased string is \"{}\"",
+        String::from_utf8_lossy(bytes.as_slice())
+    );
+}
+
+#[test]
+fn test_hexstring_from() {
+    let my_name = "Liangcheng Juves".to_string();
+    let hex = HexString.from(my_name.as_bytes());
     assert_eq!("4c69616e676368656e67204a75766573", hex);
-    println!("Convert my_name to hex string is {}", hex);
-    // let bytes = HexString.parse(hex);
-    // println!("Convert hex string to my_name is {:?}", bytes);
+}
+
+#[test]
+fn test_hexstring_parse() {
+    let hex = "4c69616e676368656e67204a75766573";
+    let bytes = HexString.parse(hex);
+    assert_eq!("Liangcheng Juves".to_string().as_bytes(), bytes);
 }
 
 struct HexString;
 
 impl HexString {
+    pub const RADIX: u32 = 16;
+
     pub fn from(&self, bytes: &[u8]) -> String {
-        let mut result = String::from("");
-        let radix: u32 = 16;
+        let mut ret = String::new();
+        let char_from_digit = |num: u32| char::from_digit(num, HexString::RADIX).unwrap();
         for byte in bytes {
             let num: u32 = *byte as u32;
-            result.push(char::from_digit(num >> 4 & 0xF, radix).unwrap());
-            result.push(char::from_digit(num & 0xF, radix).unwrap());
+            ret.push(char_from_digit(num >> 4 & 0xF));
+            ret.push(char_from_digit(num & 0xF));
         }
-        result
+        ret
     }
 
-    // pub fn parse(&self, |hex| hex: String) -> Vec<u8> {
-    //     const result_arrlen = hex.len() / 2;
-    //     let result: Vec<u8> = Vec::new();
-    //     for i in 0..result_arrlen {
-    //         let digit: u32;
-    //         digit = hex.chars().nth(2 * i).to_digit(16);
-    //         digit <<= 4;
-    //         digit += hex.chars().nth(2 * i + 1).to_digit(16);
-    //         result.push(digit)
-    //     }
-    //     result
-    // }
+    pub fn parse(&self, hex: &str) -> Vec<u8> {
+        let ret_arrlen = hex.len() / 2;
+        let mut ret: Vec<u8> = Vec::new();
+        let char_to_digit = |n: usize| {
+            hex.chars()
+                .nth(n)
+                .unwrap()
+                .to_digit(HexString::RADIX)
+                .unwrap()
+        };
+        for i in 0..ret_arrlen {
+            let mut digit: u32;
+            digit = char_to_digit(2 * i);
+            digit <<= 4;
+            digit += char_to_digit(2 * i + 1);
+            ret.push(digit as u8);
+        }
+        ret
+    }
 }
