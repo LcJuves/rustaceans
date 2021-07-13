27396615dca4fdcdaae80c0392a97a32318ae9db
邓捷w99503 <99503@sangfor.com>
2021-07-13 20:37:58 +0800

    Update demo

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,20 +1,62 @@
-/**
- * Created at 2020/11/16 22:49.
- *
- * @author Liangcheng Juves
- */
-use std::thread::sleep;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::env::current_dir;
+use std::fs::OpenOptions;
+use std::io::{ErrorKind, Read, Result};
 
-fn main() {
+trait ReadListener {
+    fn on_reading(&self, bytes: &[u8], finished: bool);
+}
+
+fn read_stream<R: Read + ?Sized, RL: ReadListener>(
+    reader: &mut R,
+    read_listener: &mut RL,
+) -> Result<()> {
+    const BUF_SIZE: usize = 1024;
+    let mut readed = Vec::<u8>::new();
+    let mut buf = [0u8; BUF_SIZE];
     loop {
-        print!(
-            "\r{}",
-            SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs()
-        );
-        sleep(Duration::new(0, 600));
+        let len = match reader.read(&mut buf) {
+            Ok(0) => {
+                read_listener.on_reading(&readed, true);
+                return Ok(());
+            }
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        readed.clear();
+        for b in &buf[..len] {
+            readed.push(*b);
+        }
+        read_listener.on_reading(&buf[..len], false);
     }
+}
+
+struct ReadListenerImpl;
+
+impl ReadListenerImpl {
+    fn new() -> ReadListenerImpl {
+        Self
+    }
+}
+
+impl ReadListener for ReadListenerImpl {
+    fn on_reading(&self, bytes: &[u8], finished: bool) {
+        if !finished {
+            print!("{}", String::from_utf8_lossy(bytes));
+        } else {
+            const SIGN: &str = "========";
+            println!();
+            println!("{} finished {}", SIGN, SIGN);
+            println!();
+            print!("{}", String::from_utf8_lossy(bytes));
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let pwd = current_dir().unwrap();
+    let path = pwd.join("Cargo.toml");
+    let mut cargo_toml = OpenOptions::new().read(true).open(path)?;
+    read_stream(&mut cargo_toml, &mut ReadListenerImpl::new())?;
+    Ok(())
 }
