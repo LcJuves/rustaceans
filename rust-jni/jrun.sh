6fe6cb82e2195c7a8db86e1c5a23b79fea97356d
Liangcheng Juves <liangchengj@outlook.com>
2021-07-24 13:46:37 +0800

    Update jrun.sh

--- rust-jni/jrun.sh
+++ rust-jni/jrun.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# Created at 2021/7/24 13:45
+# @author Liangcheng Juves
 
 base_dir="$(cd "$(dirname "$0")"; pwd)"
+run_type=release
 
-cd "${base_dir}"; cargo build --release
+cd "${base_dir}"; cargo build --${run_type}
 cd "${base_dir}/jcalls"; javac HelloWorld.java
-cd "${base_dir}/jcalls"; java HelloWorld
+cd "${base_dir}/jcalls"; java -Djava.library.path="${base_dir}/target/${run_type}" HelloWorld
