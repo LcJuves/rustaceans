91ce4297d03271f5999814cbc1ddc0ffe4ba0edd
Liangcheng Juves <liangchengj@outlook.com>
2021-07-26 17:56:40 +0800

    Updated at 2021/07/26 17:56

---
+++ hello-rocket/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/items")]
+fn items() -> &'static str {
+    "{}"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, items])
+}
