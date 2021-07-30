ed8f05d85a2692e5952fb96aedaeec70c0c7014a
Liangcheng Juves <liangchengj@outlook.com>
2021-07-30 16:17:31 +0800

    Updated at 2021/07/30 16:17

--- rfserv/src/main.rs
+++ rfserv/src/main.rs
@@ -2,7 +2,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Result;
 use std::io::Write;
-use std::net::Shutdown;
+// use std::net::Shutdown;
 use std::net::{TcpListener, TcpStream};
 use std::thread::spawn;
 
@@ -21,18 +21,17 @@ fn main() -> Result<()> {
 fn init_serv(addr: &str) -> Result<()> {
     let listener = TcpListener::bind(addr)?;
     println!("Listening on {}", addr);
-    loop {
-        let (tcp_stream, socket_addr) = listener.accept()?;
-        spawn(move || {
-            println!("Connection recevied from {}", socket_addr);
-            let out_stream = tcp_stream.try_clone()?;
-            handle_conn(tcp_stream, out_stream)
-        });
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        spawn(|| handle_conn(stream));
     }
+
+    Ok(())
 }
 
-fn handle_conn(in_stream: TcpStream, mut out_stream: TcpStream) -> Result<()> {
-    let request_lines = read_request_lines(in_stream).unwrap();
+fn handle_conn(mut stream: TcpStream) -> Result<()> {
+    let request_lines = read_request_lines(&stream)?;
     let request_lines_string = String::from_utf8(request_lines).unwrap();
     println!("{}", request_lines_string);
 
@@ -50,6 +49,8 @@ fn handle_conn(in_stream: TcpStream, mut out_stream: TcpStream) -> Result<()> {
     );
     println!("Request: {:?}", request);
 
+    // stream.shutdown(Shutdown::Read)?;
+
     /* let paths: Vec<_> = first_line_infos[1].split("/").collect();
     println!("paths: {:?}", paths);
     let mut path_buf = PathBuf::new();
@@ -58,54 +59,46 @@ fn handle_conn(in_stream: TcpStream, mut out_stream: TcpStream) -> Result<()> {
     }
     println!("path_buf: {:?}", path_buf); */
 
-    // out_stream.shutdown(Shutdown::Read)?;
-
-    // in_stream.shutdown(Shutdown::Read)?;
-
     /* if false {
         // let mut file = OpenOptions::new().read(true).open(path_buf).unwrap();
     } else { */
-    // out_stream.write_all(b"HTTP/1.1 404 Not Found\r\n")?;
-    // out_stream.write_all(b"Content-Type: text/html;charset=utf-8\r\n")?;
 
-    out_stream.write(
+    stream.write(
         b"HTTP/1.1 404 Not Found\r\n\
-Content-Type: text/plain;charset=utf-8\r\n\r\n",
+Content-Type: text/html;charset=utf-8\r\n\
+Server: Rust\r\n",
     )?;
-    out_stream.flush()?;
-    out_stream.write_all(b"404")?;
-    out_stream.flush()?;
 
-    // let mut not_found_temp_html = include_str!("not_found_temp.html").to_string();
-    // not_found_temp_html = not_found_temp_html.replace("{}", &request.uri);
+    let mut not_found_temp_html = include_str!("not_found_temp.html").to_string();
+    not_found_temp_html = not_found_temp_html.replace("{}", &request.uri);
 
-    // out_stream.write_all(b"Content-Length: ")?;
-    // out_stream.write_all(format!("{}", not_found_temp_html.as_bytes().len()).as_bytes())?;
-    // out_stream.write_all(b"\r\n\r\n")?;
+    let not_found_temp_html_bytes = not_found_temp_html.as_bytes();
 
-    // out_stream.write_all(b"404")?;
-    // out_stream.write_all(b"\r\n\r\n")?;
-    out_stream.flush()?;
+    stream.write(b"Content-Length: ")?;
+    stream.write(not_found_temp_html_bytes.len().to_string().as_bytes())?;
+    stream.write(b"\r\n\r\n")?;
+    stream.write_all(not_found_temp_html_bytes)?;
+
+    stream.flush()?;
     /* } */
 
     println!();
     println!(">>>>>> Writed");
     println!();
 
-    out_stream.shutdown(Shutdown::Write)?;
+    // stream.shutdown(Shutdown::Write)?;
 
     Ok(())
 }
 
-fn read_request_lines(stream: TcpStream) -> Result<Vec<u8>> {
+fn read_request_lines(stream: &TcpStream) -> Result<Vec<u8>> {
     let mut request_lines = Vec::<u8>::new();
-    let mut buf_reader = BufReader::new(&stream);
+    let mut buf_reader = BufReader::new(stream);
     loop {
         buf_reader.read_until(b'\n', &mut request_lines)?;
         let mut check_next_line = Vec::<u8>::new();
         buf_reader.read_until(b'\n', &mut check_next_line)?;
         if check_next_line.len() <= 2 {
-            stream.shutdown(Shutdown::Read)?;
             break;
         } else {
             request_lines.write_all(&check_next_line)?;
