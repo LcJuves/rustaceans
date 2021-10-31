40e6a66f8ecee4a0dd6658f8b5421d69a76170a8
Liangcheng Juves <liangchengj@outlook.com>
2021-10-31 18:17:08 +0800

    Updated at 2021/10/31 18:17

--- xlsx_example/src/reflection.rs
+++ xlsx_example/src/reflection.rs
@@ -1,10 +1,3 @@
-// extern crate proc_macro;
-
 pub trait Reflection {
     fn field_names() -> Vec<String>;
 }
-
-// #[proc_macro_derive(Reflection)]
-// pub fn reflection_derive(input: TokenStream) -> TokenStream {
-//     unimplemented!()
-// }
