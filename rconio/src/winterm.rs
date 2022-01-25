49bcf91da1c79057169fb3b5a6742020e61cfbf8
Liangcheng Juves <liangchengj@outlook.com>
2022-01-25 11:52:03 +0800

    Updated at 2022/01/25 11:52

--- rconio/src/winterm.rs
+++ rconio/src/winterm.rs
@@ -4,7 +4,8 @@
 
 use core::ffi::c_void;
 
-use std::ptr::null_mut;
+use std::process::Command;
+use std::ptr::{null, null_mut};
 use std::sync::Once;
 
 use lazy_static::lazy_static;
@@ -13,12 +14,17 @@ use windows::Win32::Foundation::HANDLE;
 use windows::Win32::System::Console::GetConsoleMode;
 use windows::Win32::System::Console::GetConsoleScreenBufferInfo;
 use windows::Win32::System::Console::GetStdHandle;
+use windows::Win32::System::Console::ScrollConsoleScreenBufferW;
+use windows::Win32::System::Console::SetConsoleCursorPosition;
 use windows::Win32::System::Console::SetConsoleMode;
 use windows::Win32::System::Console::SetConsoleTextAttribute;
 use windows::Win32::System::Console::WriteConsoleW;
+use windows::Win32::System::Console::CHAR_INFO;
 use windows::Win32::System::Console::CONSOLE_MODE;
 use windows::Win32::System::Console::CONSOLE_SCREEN_BUFFER_INFO;
+use windows::Win32::System::Console::COORD;
 use windows::Win32::System::Console::ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+use windows::Win32::System::Console::SMALL_RECT;
 use windows::Win32::System::Console::STD_OUTPUT_HANDLE;
 
 lazy_static! {
@@ -34,6 +40,12 @@ lazy_static! {
             .unwrap_or(());
         }
         buf_info.wAttributes
+    };
+    static ref IS_CYGWIN_FAMILY: bool = {
+        if let Ok(status) = Command::new("cygpath").arg("-V").status() {
+            return status.success();
+        }
+        false
     };
     pub(crate) static ref STDOUT_HANDLE: HANDLE = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
 }
@@ -136,11 +148,61 @@ pub(crate) fn set_under_line() {
     }
 }
 
-// pub(crate) fn clear_screen() {
-//     if let Err(_) = write_conw("\x1bc") {
-//         todo!();
-//     }
-// }
+pub(crate) fn cls() {
+    if let Err(_) = write_conw("\x1bc") {
+        let is_cygwin_family = *IS_CYGWIN_FAMILY;
+        if is_cygwin_family {
+            print!("\x1bc");
+        } else {
+            let stdout_handle = get_stdout_handle();
+            let mut csbi: CONSOLE_SCREEN_BUFFER_INFO = CONSOLE_SCREEN_BUFFER_INFO::default();
+            let mut scroll_rect = SMALL_RECT::default();
+            let mut scroll_target = COORD::default();
+            let mut fill = CHAR_INFO::default();
+
+            unsafe {
+                // Get the number of character cells in the current buffer.
+                if !GetConsoleScreenBufferInfo(
+                    stdout_handle,
+                    &mut csbi as *mut CONSOLE_SCREEN_BUFFER_INFO,
+                )
+                .as_bool()
+                {
+                    return;
+                }
+
+                // Scroll the rectangle of the entire buffer.
+                scroll_rect.Left = 0;
+                scroll_rect.Top = 0;
+                scroll_rect.Right = csbi.dwSize.X;
+                scroll_rect.Bottom = csbi.dwSize.Y;
+
+                // Scroll it upwards off the top of the buffer with a magnitude of the entire height.
+                scroll_target.X = 0;
+                scroll_target.Y = 0 - csbi.dwSize.Y;
+
+                // Fill with empty spaces with the buffer's default text attribute.
+                fill.Char.UnicodeChar = ' ' as u16;
+                fill.Attributes = csbi.wAttributes;
+
+                // Do the scroll
+                ScrollConsoleScreenBufferW(
+                    stdout_handle,
+                    &scroll_rect,
+                    null() as *const SMALL_RECT,
+                    scroll_target,
+                    &fill,
+                );
+
+                // Move the cursor to the top left corner too.
+                csbi.dwCursorPosition.X = 0;
+                csbi.dwCursorPosition.Y = 0;
+
+                SetConsoleCursorPosition(stdout_handle, csbi.dwCursorPosition);
+            }
+        }
+    }
+}
 
 pub(crate) fn println(r#str: &str) {
     if let Err(_) = write_conw(r#str) {
