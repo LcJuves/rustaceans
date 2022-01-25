e1e48fffad60ffdaabc0350d88ae94604047a819
Liangcheng Juves <liangchengj@outlook.com>
2022-01-25 17:56:54 +0800

    Updated at 2022/01/25 17:56

--- rconio/src/vtesc_seq.rs
+++ rconio/src/vtesc_seq.rs
@@ -7,21 +7,21 @@ pub const BLANKING: &str = "\x1b[8m";
 pub const CLEAR_SCREEN: &str = "\x1bc";
 pub const CLEAR_CONTENT_FROM_CURSOR_OF_EOL: &str = "\x1b[K";
 
-pub const RED: &str = "\x1b[0;32;31m";
-pub const GREEN: &str = "\x1b[0;32;32m";
-pub const BLUE: &str = "\x1b[0;32;34m";
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const BLUE: &str = "\x1b[34m";
 
-pub const WHITE: &str = "\x1b[1;37m";
-pub const CYAN: &str = "\x1b[0;36m";
-pub const PURPLE: &str = "\x1b[0;35m";
-pub const YELLOW: &str = "\x1b[1;33m";
-pub const BROWN: &str = "\x1b[0;33m";
+pub const WHITE: &str = "\x1b[37m";
+pub const CYAN: &str = "\x1b[36m";
+pub const PURPLE: &str = "\x1b[35m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const BROWN: &str = "\x1b[33m";
 
-pub const LIGHT_RED: &str = "\x1b[1;31m";
-pub const LIGHT_GREEN: &str = "\x1b[1;32m";
-pub const LIGHT_BLUE: &str = "\x1b[1;34m";
-pub const LIGHT_CYAN: &str = "\x1b[1;36m";
-pub const LIGHT_PURPLE: &str = "\x1b[1;35m";
-pub const LIGHT_GRAY: &str = "\x1b[0;37m";
+pub const LIGHT_RED: &str = "\x1b[31m";
+pub const LIGHT_GREEN: &str = "\x1b[32m";
+pub const LIGHT_BLUE: &str = "\x1b[34m";
+pub const LIGHT_CYAN: &str = "\x1b[36m";
+pub const LIGHT_PURPLE: &str = "\x1b[35m";
+pub const LIGHT_GRAY: &str = "\x1b[37m";
 
-pub const DARK_GRAY: &str = "\x1b[1;30m";
+pub const DARK_GRAY: &str = "\x1b[30m";
