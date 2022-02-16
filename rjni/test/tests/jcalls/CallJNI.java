5b680db2cae59f07c859b32adcf19341520fe92c
Liangcheng Juves <liangchengj@outlook.com>
2022-02-16 21:02:34 +0800

    Reformat jcalls code

--- rjni/test/tests/jcalls/CallJNI.java
+++ rjni/test/tests/jcalls/CallJNI.java
@@ -7,16 +7,16 @@ import java.io.PrintStream;
 
 class CallJNI {
 
-  static {
-    System.loadLibrary("rjnit");
-    System.out.println();
-  }
+    static {
+        System.loadLibrary("rjnit");
+        System.out.println();
+    }
 
-  static native int getVersion();
+    static native int getVersion();
 
-  static native Class<?> defineClass(String name, ClassLoader loader, byte[] buf, int len);
+    static native Class<?> defineClass(String name, ClassLoader loader, byte[] buf, int len);
 
-  static native Class<?> findClass(String name);
+    static native Class<?> findClass(String name);
 
-  static native PrintStream getSystemOut();
+    static native PrintStream getSystemOut();
 }
--- rjni/test/tests/jcalls/Main.java
+++ rjni/test/tests/jcalls/Main.java
@@ -3,51 +3,51 @@ import java.io.PrintStream;
 /** Main */
 class Main {
 
-  private static void println(Object x) {
-    System.out.println(x);
-  }
-
-  private static void eprintln(Object x) {
-    System.err.println(x);
-  }
-
-  private static void pass() {
-    System.out.print("\033[32;1m");
-    System.out.print("PASS");
-    System.out.print("\033[m");
-    System.out.println();
-  }
-
-  private static <E> void assertType(E x, Class<?> clazz) {
-    if (clazz != x.getClass()) {
-      throw new AssertionError();
+    private static void println(Object x) {
+        System.out.println(x);
     }
-  }
 
-  public static void main(String[] args) {
-    for (; ; ) {
-      try {
-        int version = CallJNI.getVersion();
-        assertType(version, Integer.class);
-        println(String.format("Version: %s", version));
-        pass();
-
-        Class<?> clazz = CallJNI.findClass("java/lang/String");
-        assertType(clazz, String.class.getClass());
-        println(clazz.getName());
-        pass();
-
-        PrintStream out = (PrintStream) CallJNI.getSystemOut();
-        assertType(out, PrintStream.class);
-        out.println("CallJNI: System.out >>>");
-        pass();
-
-        break;
-      } catch (Throwable tr) {
-        tr.printStackTrace();
-        eprintln("FAILED");
-        break;
-      }
+    private static void eprintln(Object x) {
+        System.err.println(x);
     }
-  }
+
+    private static void pass() {
+        System.out.print("\033[32;1m");
+        System.out.print("PASS");
+        System.out.print("\033[m");
+        System.out.println();
+    }
+
+    private static <E> void assertType(E x, Class<?> clazz) {
+        if (clazz != x.getClass()) {
+            throw new AssertionError();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (; ; ) {
+            try {
+                int version = CallJNI.getVersion();
+                assertType(version, Integer.class);
+                println(String.format("Version: %s", version));
+                pass();
+
+                Class<?> clazz = CallJNI.findClass("java/lang/String");
+                assertType(clazz, String.class.getClass());
+                println(clazz.getName());
+                pass();
+
+                PrintStream out = (PrintStream) CallJNI.getSystemOut();
+                assertType(out, PrintStream.class);
+                out.println("CallJNI: System.out >>>");
+                pass();
+
+                break;
+            } catch (Throwable tr) {
+                tr.printStackTrace();
+                eprintln("FAILED");
+                break;
+            }
+        }
+    }
 }
