c9ec61e24e370fb39098816ee447eef42db72fc5
Liangcheng Juves <liangchengj@outlook.com>
2022-05-07 15:26:33 +0800

    Updated at 2022/05/07 15:26

--- fsplit/src/main.rs
+++ fsplit/src/main.rs
@@ -1,3 +1,9 @@
+pub(crate) fn compute_sha512sum(bytes: &[u8]) -> String {
+    let integrity = IntegrityOpts::new().algorithm(Algorithm::Sha512).chain(bytes).result();
+    let (_, hex) = integrity.to_hex();
+    hex
+}
+
 fn main() {
     println!("Hello, world!");
 }
